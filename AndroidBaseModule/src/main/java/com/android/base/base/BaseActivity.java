24f4e0dc9d253320fa5c0aee9ec68305f28ab6fb
jiangzg <727918130@qq.com>
2017-04-10 22:35:03 +0800

    update baseActivity

--- AndroidBaseModule/src/main/java/com/android/base/base/BaseActivity.java
+++ AndroidBaseModule/src/main/java/com/android/base/base/BaseActivity.java
@@ -3,22 +3,31 @@ package com.android.base.base;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.Fade;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 
 import com.android.base.R;
 import com.android.base.utils.comp.ActivityUtils;
 import com.android.base.utils.func.AnalyUtils;
-import com.android.base.utils.view.DialogUtils;
 import com.android.base.utils.net.NetUtils;
+import com.android.base.utils.view.DialogUtils;
+import com.android.base.utils.view.ScreenUtils;
 
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -29,60 +38,89 @@ import butterknife.Unbinder;
  */
 public abstract class BaseActivity<T> extends AppCompatActivity {
 
-    /* 子类复制类似方法 实现跳转 */
+    public BaseActivity mActivity;
+    public FragmentManager mFragmentManager;
+    private ProgressDialog loading;
+    private ProgressDialog progress;
+    private Unbinder unbinder;
+
+    /* activity跳转demo */
     private static void goActivity(Activity from) {
         Intent intent = new Intent(from, BaseActivity.class);
         // intent.putExtra();
         ActivityUtils.startActivity(from, intent);
     }
 
-    /* 子类复制类似方法 实现跳转 */
+    /* activity跳转demo */
     private static void goActivity(Fragment from) {
         Intent intent = new Intent(from.getActivity(), BaseActivity.class);
         // intent.putExtra();
         ActivityUtils.startActivity(from, intent);
     }
 
-    public String logTag = "BaseActivity";
-    public BaseActivity mActivity;
-    public FragmentManager mFragmentManager;
-    public ProgressDialog loading;
-    public ProgressDialog progress;
-    public Intent mIntent;
-    private Unbinder unbinder;
+    /**
+     * @return 对话框(静态会混乱)
+     */
+    public ProgressDialog getLoading() {
+        if (loading != null) return loading;
+        loading = DialogUtils.createLoading(mActivity,
+                0, "", getString(R.string.wait), true);
+        return loading;
+    }
 
-    /* 初始layout(setContent之前调用) */
-    protected abstract int initObj(Bundle savedInstanceState);
+    /**
+     * @return 进度框(静态会混乱)
+     */
+    public ProgressDialog getProgress() {
+        if (progress != null) return progress;
+        progress = DialogUtils.createProgress(mActivity,
+                0, getString(R.string.wait), true, 100, 0, null);
+        return progress;
+    }
 
-    /* 实例化View */
-    protected abstract void initView();
+    /**
+     * @return 获取当前类(影响性能, 所以需要被动获取)
+     */
+    @SuppressWarnings("unchecked")
+    public Class<T> getCls() {
+        Type type = this.getClass().getGenericSuperclass();
+        return (Class<T>) (((ParameterizedType) (type)).getActualTypeArguments()[0]);
+    }
 
-    /* 初始Data */
-    protected abstract void initData();
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        super.setContentView(layoutResID);
+        unbinder = ButterKnife.bind(this);
+        // 需要重新initView()
+    }
+
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(view);
+        unbinder = ButterKnife.bind(this);
+        // 需要重新initView()
+    }
+
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params) {
+        super.setContentView(view, params);
+        unbinder = ButterKnife.bind(this);
+        // 需要重新initView()
+    }
+
+    @Override
+    public void addContentView(View view, ViewGroup.LayoutParams params) {
+        super.addContentView(view, params);
+        unbinder = ButterKnife.bind(this);
+        // 需要重新initView()
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        logTag = getCls();
         mActivity = this;
-        ActivityUtils.initBeforeSuperCreate(mActivity);
+        initBeforeSuperCreate(mActivity);
         super.onCreate(savedInstanceState);
         mFragmentManager = getSupportFragmentManager();
-        loading = DialogUtils.createLoading(mActivity,
-                0, "", getString(R.string.wait), true);
-        progress = DialogUtils.createProgress(mActivity,
-                0, getString(R.string.wait), true, 100, 0, null);
-        mIntent = getIntent();
-        setContentView(initObj(savedInstanceState)); // 这之后 页面才会加载出来
-    }
-
-    /* setContentView()或addContentView()后调用,view只是加载出来，没有实例化.
-     * 为了页面的加载速度，不要在onContentChanged前做过多的操作 */
-    @Override
-    public void onContentChanged() {
-        super.onContentChanged();
-        unbinder = ButterKnife.bind(this); // 每次setContentView之后都要bind一下
-        initView(); // 二次setContentView之后控件是以前view的，所以要重新实例化一次
-        initData(); // 必须保证initView执行完，onStart不行
     }
 
     @Override
@@ -143,12 +181,29 @@ public abstract class BaseActivity<T> extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /* 获取当前类的 */
-    @SuppressWarnings("unchecked")
-    private String getCls() {
-        Class<T> cls = (Class<T>) (((ParameterizedType) (this.getClass()
-                .getGenericSuperclass())).getActualTypeArguments()[0]);
-        return cls.getSimpleName();
+    /* activity初始化 */
+    private void initBeforeSuperCreate(Activity activity) {
+        Window window = activity.getWindow();
+        // 键盘不会遮挡输入框
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        // 不自动弹键盘
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        // 总是隐藏键盘
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        // 竖屏
+        ScreenUtils.requestPortrait(activity);
+        // titleBar
+        if (activity instanceof AppCompatActivity) {
+            ScreenUtils.requestNoTitle((AppCompatActivity) activity);
+        }
+        // 专门的跳转方式才会有过场效果
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+            window.setEnterTransition(new Fade()); // 下一个activity进场
+            window.setExitTransition(new Fade()); //  当前activity向后时退场
+            // window.setReenterTransition(slideIn); // 上一个activity进场
+            // window.setReturnTransition(slideOut); // 当前activity向前时退场
+        }
     }
 
 }
