52328f726bd5fca0e4f9ed36b74317ca2ba00422
jiangzg <727918130@qq.com>
2017-05-08 21:14:20 +0800

    update moudle

--- AndroidBaseNative/src/main/java/com/android/base/media/PlayerUtils.java
+++ AndroidBaseNative/src/main/java/com/android/base/media/PlayerUtils.java
@@ -1,11 +1,4 @@
-package com.android.base.utils.media;
-
-import android.content.Context;
-import android.media.MediaPlayer;
-import android.net.Uri;
-import android.util.Log;
-
-import java.io.IOException;
+package com.android.base.media;
 
 /**
  * Created by gg on 2017/4/3.
---
+++ AndroidBaseNative/src/main/java/com/android/base/sync/JThread.java
@@ -0,0 +1,25 @@
+package com.android.base.sync;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+/**
+ * Created by gg on 2017/5/8.
+ * 线程管理类
+ */
+public class JThread {
+
+    private void test() {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        ExecutorService executorService1 = Executors.newFixedThreadPool(1);
+        ExecutorService executorService2 = Executors.newSingleThreadExecutor();
+
+//        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(,,,,);
+
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        ScheduledExecutorService scheduledExecutorService1 = Executors.newSingleThreadScheduledExecutor();
+    }
+
+
+}
